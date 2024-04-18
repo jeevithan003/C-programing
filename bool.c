d284b5c36aca21a4d13a0f3ea7258fd3d9bd2f92
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-18 18:21:33 +0530

    Create bool.c

---
+++ bool.c
@@ -0,0 +1,41 @@
+// C Program to implement
+// Boolean data type
+#include <stdbool.h>
+
+// Main Function
+int main()
+{
+	// Boolean data types declared
+	bool a = true;
+	bool b = false;
+
+	printf("True : %d\n", a);
+	printf("False : %d", b);
+
+	return 0;
+}
+// C Program to implement
+// conditional statements
+#include <stdbool.h>
+#include <stdio.h>
+
+// Main Function
+int main()
+{
+
+	// Integers declared
+	int a = 3;
+	int b = 4;
+
+	// Conditional Statements
+	if (a > b) {
+		printf("a is greater\n");
+	}
+	else {
+		printf("a is smaller\n");
+	}
+
+	printf("%d is the result of a>b", a > b);
+
+	return 0;
+}
