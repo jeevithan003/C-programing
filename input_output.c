2e6f3a36ab3f06fc645407be183b81945ba05022
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-19 22:07:34 +0530

    Create input_output.c

---
+++ input_output.c
@@ -0,0 +1,79 @@
+// C program to show input and output
+
+#include <stdio.h>
+
+int main()
+{
+
+    // Declare the variables
+    int num;
+    char ch;
+    float f;
+
+    // --- Integer ---
+
+    // Input the integer
+    printf("Enter the integer: ");
+    scanf("%d", &num);
+
+    // Output the integer
+    printf("\nEntered integer is: %d", num);
+
+    // --- Float ---
+  
+    //For input Clearing buffer
+      while((getchar()) != '\n');
+
+    // Input the float
+    printf("\n\nEnter the float: ");
+    scanf("%f", &f);
+
+    // Output the float
+    printf("\nEntered float is: %f", f);
+
+    // --- Character ---
+
+    // Input the Character
+    printf("\n\nEnter the Character: ");
+    scanf("%c", &ch);
+
+    // Output the Character
+    printf("\nEntered character is: %c", ch);
+
+    return 0;
+}
+
+
+// C program to show input and output
+ 
+#include <stdio.h>
+ 
+int main()
+{
+ 
+    // Declare string variable
+    // as character array
+    char str[50];
+ 
+    // --- String ---
+    // To read a word
+ 
+    // Input the Word
+    printf("Enter the Word: ");
+    scanf("%s\n", str);
+ 
+    // Output the Word
+    printf("\nEntered Word is: %s", str);
+ 
+    // --- String ---
+    // To read a Sentence
+ 
+    // Input the Sentence
+    printf("\n\nEnter the Sentence: ");
+    scanf("%[^\n]s", str);
+ 
+    // Output the String
+    printf("\nEntered Sentence is: %s", str);
+ 
+    return 0;
+}
