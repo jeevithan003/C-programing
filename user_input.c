9741a4d76d27523c9abbbb3ed31b3e925b812281
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-25 00:18:53 +0530

    Create user_input.c

---
+++ user_input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    char name[232];
+    printf("enter your name\n");
+    scanf("%s",&name);
+    printf("nect block\n");
+    printf("your name is %s",name);
+    return 0;
+}
