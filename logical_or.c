128483dd2f39e730c33731d2b5d5786a38efcb95
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-26 00:10:31 +0530

    Create logical_or.c

---
+++ logical_or.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    //logical opertor in c
+    //and or not
+    //&& || !
+    int a=10;
+    int b=2430;
+    if (a>2 || b<50){
+        printf("using and && opertor");
+    }
+    else{
+        printf("&& operator failed in this case\n");
+    }
+}
