67fa58881a12b910d25131c3233bc8547cc7829e
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-26 00:11:45 +0530

    Create logical_not.c

---
+++ logical_not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    //logical opertor in c
+    //and or not
+    //&& || !
+    int a=10;
+    int b=2430;
+    if (a!=20){
+        printf("using not operator");
+    }
+    else{
+        printf("&& operator failed in this case\n");
+    }
+}
